c0345fce80ce217c90be31db4c847cfdea053205
Chetan <Chetan@DESKTOP-9SU09BS>
2020-11-02 19:51:13 +0530

    [Chetan]Add. Add contact through console.

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -11,8 +11,26 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to Address Book System...!");
+            ///Created the object of address book class
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact("Kunal", "Sharma", "Dadar", " Mumbai", "Maharashtra", 400084, 9632587321, "kunal@gmail.com");
+            ///Reads details from user.
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            addressBook.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
             addressBook.DisplayContact();
         }
     }
